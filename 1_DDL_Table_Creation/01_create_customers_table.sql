20bff3558b049bdf25f5c6e20417b4b0d80e3e74
Meenu0103 <meenuvinayagam01@gmail.com>
2025-12-05 15:38:48 +0530

    Create 01_create_customers_table.sql

---
+++ 1_DDL_Table_Creation/01_create_customers_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE olist_customers (
+    customer_id varchar(50),
+    customer_unique_id varchar(50),
+    customer_zip_code_prefix int,
+    customer_city varchar(100),
+    customer_state varchar(5)
+);
