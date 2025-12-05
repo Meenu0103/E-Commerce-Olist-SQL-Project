bcad62b4d5a2aa5fc21acfb055788f44a7794cc1
Meenu0103 <meenuvinayagam01@gmail.com>
2025-12-05 15:54:50 +0530

    07_create_geolocation_table.sql
    Added geolocation table creation script

---
+++ 1_DDL_Table_Creation/07_create_geolocation_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE olist_geolocation (
+    geolocation_zip_code_prefix INT,
+    geolocation_lat NUMERIC(10,6),
+    geolocation_lng NUMERIC(10,6),
+    geolocation_city VARCHAR(100),
+    geolocation_state VARCHAR(5)
+);
